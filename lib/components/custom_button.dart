71c866e952e7917354dec3c634ca994094932bae
manuelcastrobarinas <manuel.castrobarinas@gmail.com>
2026-02-13 08:49:33 -0500

    se creo la primera pantalla y apartir de ella se enseño el diseño del appbar

---
+++ lib/components/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+
+  final IconData icon;
+
+  const CustomButton({super.key, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(33, 27, 68, 1),
+        borderRadius: BorderRadius.circular(100),
+        border: Border.all(color: const Color.fromARGB(255, 100, 100, 100), width: 2),
+      ),
+      child: IconButton(
+        onPressed: () {
+          print("hola mundo");
+        }, 
+        icon: Icon(icon)
+      ),
+    );
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+Map<String, WidgetBuilder> routes = {
+  "/": (context) => const HomePage(),
+};
