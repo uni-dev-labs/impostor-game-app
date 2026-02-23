b55b79c4bd4db8a9d466dac77231316ab9921dba
manuelcastrobarinas <manuel.castrobarinas@gmail.com>
2026-02-23 15:31:45 -0500

    implenete mas contenedores y tambien implemente el scroll y le di el tamaño a la pantalla, aparte añadi una imagen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,38 +7,72 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final Size size =MediaQuery.of(context).size; //TODO: OPTIMIZAR ESTE CALCULO
+
     return Scaffold(
       backgroundColor: Colors.purple,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              Container(
-                width : double.infinity,
-                height: 100,
-                color: Colors.transparent,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomButton(icon: Icons.settings),
-                    CustomButton(icon: Icons.info_outline),
-                  ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width : double.infinity,
+                  height: 100,
+                  color: Colors.red,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(icon: Icons.settings),
+                      CustomButton(icon: Icons.info_outline),
+                    ],
+                  ),
                 ),
-              ),
-              Text("El \n Impostor", style: TextStyle(
-                color: Colors.white, fontSize: 24,
-                fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              Text("DESCUBRE QUIÉN ES EL IMPOSTOR", style: TextStyle(
-                  color: Colors.grey, fontSize: 16,
-                  fontWeight: FontWeight.bold
+                Text("El \n Impostor", style: TextStyle(
+                  color: Colors.white, fontSize: 24,
+                  fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-            ],
+                Text("DESCUBRE QUIÉN ES EL IMPOSTOR", style: TextStyle(
+                    color: Colors.grey, fontSize: 16,
+                    fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      fit: BoxFit.contain,
+                    ),
+                  )
+                ),
+                Container(
+                  width: double.infinity,
+                  height: size.height * 0.3,
+                  color: Colors.green,
+                  child: Text("Iniciar"),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: size.height * 0.7,
+                  color: Colors.yellow,
+                  child: Text("Iniciar"),
+                ),
+              ],
+            ),
           ),
         ),
       )
