392942778dd39d5512789b8e5d1df2714c913d18
Jhon <jhon1773@github.com>
2026-02-25 16:19:09 -0500

    Set app background to purple

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Impostor',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.red),
+        colorScheme: .fromSeed(seedColor: Colors.purple),
+        scaffoldBackgroundColor: Colors.purple,
       ),
       routes: routes,
       initialRoute: "/",
