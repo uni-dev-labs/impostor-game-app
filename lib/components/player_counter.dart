14ae553535af74f5317b17829771fb3aeed85bc7
AngelicaPD <angelica.puentes@usantoto.edu.co>
2026-02-27 14:02:58 -0500

    avance contador de jugadores y pantalla config

--- lib/components/player_counter.dart
+++ lib/components/player_counter.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-class PlayerCounter extends StatelessWidget {
+class PlayerCounter extends StatefulWidget {
   const PlayerCounter({super.key});
 
   @override
+  State<PlayerCounter> createState() => _PlayerCounterState();
+}
+
+class _PlayerCounterState extends State<PlayerCounter> {
+  int players = 3;
+  final int maxPlayers = 20;
+
+  @override
   Widget build(BuildContext context) {
+    double progress = players / maxPlayers;
+
     return Container(
       decoration: BoxDecoration(
         color: const Color.fromRGBO(17, 20, 39, 1),
@@ -16,47 +26,84 @@ class PlayerCounter extends StatelessWidget {
       ),
       width: double.infinity,
       height: 120,
-
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(              
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(32, 40, 59, 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: const Icon(Icons.remove, color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            /// BOTÓN MENOS
+            GestureDetector(
+              onTap: () {
+                if (players > 3) {
+                  setState(() {
+                    players--;
+                  });
+                }
+              },
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(32, 40, 59, 1),
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                SizedBox(width: 10),
-                Container(
-                  height: 10,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(32, 40, 59, 1),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  
-                ),
-                SizedBox(width: 10),              
-                Container(
-                  height: 50,
-                  width: 50,                  
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(55, 20, 234, 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: const Icon(Icons.add, color: Colors.white),
-                ),
-              ],
+                child: const Icon(Icons.remove, color: Colors.white),
+              ),
             ),
-          ),
-        ],
+
+            const SizedBox(width: 10),
+
+            /// BARRA DE PROGRESO
+            Expanded(
+              child: Stack(
+                alignment: Alignment.centerLeft,
+                children: [
+                  // Fondo
+                  Container(
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(32, 40, 59, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+
+                  // Progreso
+                  FractionallySizedBox(
+                    widthFactor: progress,
+                    child: Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: const Color.fromRGBO(55, 20, 234, 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),                
+                ],
+              ),
+            ),
+
+            const SizedBox(width: 10),
+
+            /// BOTÓN MÁS
+            GestureDetector(
+              onTap: () {
+                if (players < maxPlayers) {
+                  setState(() {
+                    players++;
+                  });
+                }
+              },
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(55, 20, 234, 1),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Icon(Icons.add, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
